a796e48f67af976036f538a5ab285111d72a85cd
Yusuf <yshaikh113@outlook.com>
2024-11-15 10:07:43 -0600

    responsiveness

--- portfolio/src/components/Navbar/Navbar.jsx
+++ portfolio/src/components/Navbar/Navbar.jsx
@@ -1,17 +1,29 @@
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
-import { FaExternalLinkAlt } from 'react-icons/fa'
+import { FaExternalLinkAlt, FaBars, FaTimes } from 'react-icons/fa'
 import styles from './Navbar.module.css'
 
-
 function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen)
+  }
+
   return (
     <header>
-        <nav>
-            <ul>
-            <li><Link to='/' className={styles['nav-btn']}>Home</Link></li>
-            <li><Link to='/resume' className={styles['nav-btn']}>Resume</Link></li>
-            <li><Link to='/notes' className={styles['nav-btn']}>Notes</Link></li>
-            <li>
+      <nav>
+        {/* Center the hamburger icon */}
+        <div className={styles['hamburger-icon']} onClick={toggleMenu}>
+          {isMenuOpen ? <FaTimes size={30} color="white" /> : <FaBars size={30} color="white" />}
+        </div>
+
+        {/* Hide menu items when the menu is open */}
+        <ul className={`${styles.navList} ${isMenuOpen ? styles.showMenu : ''}`}>
+          <li><Link to='/' className={styles['nav-btn']}>Home</Link></li>
+          <li><Link to='/resume' className={styles['nav-btn']}>Resume</Link></li>
+          <li><Link to='/notes' className={styles['nav-btn']}>Notes</Link></li>
+          <li>
             <a 
               href='https://github.com/yfshaikh' 
               target='_blank' 
@@ -31,8 +43,8 @@ function Navbar() {
               LinkedIn <FaExternalLinkAlt className={styles['external-link-icon']} />
             </a>
           </li>
-            </ul>
-        </nav>
+        </ul>
+      </nav>
     </header>
   )
 }
